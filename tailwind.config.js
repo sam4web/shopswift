50dacd18cd31100d156c823c46b041475ab883c0
Sijal Manandhar <sijal.m06@gmail.com>
2024-01-18 02:41:17 +0545

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html', './static/**/*.{css, js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
