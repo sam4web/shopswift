e37a986a12c5cc68496d0f1d463b657ac7751603
Sijal Manandhar <sijal.m06@gmail.com>
2024-02-06 21:48:39 +0545

    frontend code to django > header & footer into django components > index and about page setup > images added > script & tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./template/**/*.html', './static/**/*.{css,js}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0069E0',
+        'secondary-dark': '#021431',
+        'primary-dark': '#181920',
+        'body-dark': '#272935',
+        gray: { DEFAULT: '#394E6A', dark: '#414558' },
+        light: '#F0F6FF',
+      },
+    },
+    fontFamily: {
+      heading: ['Nunito', 'sans-serif'],
+      display: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 };
-
-// npx tailwindcss -i ./static/css/main.css -o ./static/css/main.min.css --watch
