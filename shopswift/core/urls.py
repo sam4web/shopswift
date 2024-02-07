af8c2204c43c93dbc0fef83df554977fc6c0e2ca
Sijal Manandhar <sijal.m06@gmail.com>
2024-02-07 16:57:26 +0545

    static pages & urls setup

--- shopswift/core/urls.py
+++ shopswift/core/urls.py
@@ -5,4 +5,14 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("order/", views.order, name="order"),
+    path("user/profile/", views.profile, name="profile"),
+    path("user/login/", views.login, name="login"),
+    path("user/register/", views.register, name="register"),
+    path("products/", views.product_list, name="product_list"),
+    path("products/<int:pk>/", views.product_detail, name="product_detail"),
+    path("products/create/", views.product_create, name="product_create"),
 ]
--- shopswift/core/views.py
+++ shopswift/core/views.py
@@ -4,3 +4,43 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "core/index.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def product_list(request):
+    return render(request, "product/list.html")
+
+
+def product_detail(request, pk):
+    return render(request, "product/detail.html")
+
+
+def product_create(request):
+    return render(request, "product/create.html")
+
+
+def cart(request):
+    return render(request, "core/cart.html")
+
+
+def checkout(request):
+    return render(request, "core/checkout.html")
+
+
+def order(request):
+    return render(request, "core/order.html")
+
+
+def profile(request):
+    return render(request, "user/profile.html")
+
+
+def login(request):
+    return render(request, "user/login.html")
+
+
+def register(request):
+    return render(request, "user/register.html")
